d67b9029fe941146155eb26a044c2325f4f92fc6
Game2njoy <sg851033@gmail.com>
2023-06-13 03:05:33 +0900

    도메인

--- RealEstate/settings/prod.py
+++ RealEstate/settings/prod.py
@@ -1,4 +1,4 @@
 from .base import *
 
-ALLOWED_HOSTS = ['52.78.30.23', 'xn--or3b91dmzhwwci2mikh.com']
+ALLOWED_HOSTS = ['52.78.30.23', 'xn--or3b91dmzhwwci2mikh.com', 'www.xn--or3b91dmzhwwci2mikh.com']
 DEBUG = False
